c4a2aa72e32398e498eee05d63a73c0dce7a21ab
ricnox <ricardo.baruttis.external@gmail.com>
2019-07-11 11:24:12 +0100

    probando

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+/* Populate tables /*
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Guzean', 'profesor@bolsadeideas.com', '2017-08-20');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-20');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(3, 'Andres2', 'Guzean', 'profesor@bolsadeideas.com', '2017-08-20');
